4d0aae766b5ba09192d823ae372b7d0a54bf9e5f
Mattwmaster58 <mattwmaster58@gmail.com>
2022-06-22 13:02:51 -0400

    fix: module name typo

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     install_requires=open("requirements.txt").read().split("\n"),
     entry_points={
     'console_scripts': [
-        'overlay = overlay.cli:cli',
+        'overlay = overlay.main:cli',
     ]}
 )
 

6e6af20d96db16ed36736567dea67c2fd9532598
Mattwmaster58 <mattwmaster58@gmail.com>
2022-06-22 12:58:21 -0400

    freehand setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='overlay',
+    version='0.1',
+    py_modules=['overlay'],
+    install_requires=open("requirements.txt").read().split("\n"),
+    entry_points={
+    'console_scripts': [
+        'overlay = overlay.cli:cli',
+    ]}
+)
+
+
